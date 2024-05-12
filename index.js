af8ba07146c334fcce302986c5f902135980c489
dtefwuh <gs1erionlol@hotmail.com>
2024-05-12 22:01:50 +0700

    update project

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-function LRUCache(capacity) {
-  this.capacity = capacity;
-  this.cache = new Map();
+function sumArray(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0);
 }
-LRUCache.prototype.get = function (key) {
-  if (!this.cache.has(key)) return -1;
-  const value = this.cache.get(key);
-  this.cache.delete(key);
-  this.cache.set(key, value);
-  return value;
-};
-LRUCache.prototype.put = function (key, value) {
-  if (this.cache.has(key)) this.cache.delete(key);
-  if (this.cache.size === this.capacity) {
-    const firstKey = this.cache.keys().next().value;
-    this.cache.delete(firstKey);
-  }
-  this.cache.set(key, value);
-};
